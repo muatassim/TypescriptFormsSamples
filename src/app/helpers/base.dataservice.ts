cb5dbcb9ecd656af8d0482c147f316a28419b1ec
muatassim@hotmail.com <muatassim@hotmail.com>
2021-02-09 17:16:56 -0500

    added axios sample

--- src/app/helpers/base.dataservice.ts
+++ src/app/helpers/base.dataservice.ts
@@ -1,21 +1,16 @@
 import Axios, { AxiosResponse } from "axios";
-import { Spinner } from "spin.js";
-import { IUtility, Utility } from "../helpers/utility";
-import {ValidationError } from "../models/validationerror";
 interface IBaseDataService{
     Get<T>(url:string): Promise<AxiosResponse>;
     Add<T,FormData>(obj:T, contentType:string,url:string): Promise<AxiosResponse>;
     Update<T,FormData>(obj:T, contentType:string,url:string): Promise<AxiosResponse>;
     Remove(contentType:string,url:string): Promise<AxiosResponse>;
-    handleError(reason:any):void;
     handleResponse(response:AxiosResponse) : void;
-    HandleMethods(responseAx:Promise<AxiosResponse>,spinner: Spinner):void;
+    HandleMethods(responseAx:Promise<AxiosResponse>):void;
 }
 
 class BaseDataService implements IBaseDataService{
-    utility:IUtility; 
+
     constructor(){
-        this.utility =new Utility(); 
     }
     Get<T>(url:string): Promise<AxiosResponse>  { 
         let data = Axios.get<T>(url);
@@ -25,25 +20,23 @@ class BaseDataService implements IBaseDataService{
     Add<T>(obj:T, contentType:string,url:string): Promise<AxiosResponse>  {
         const config = { headers: { 'Content-Type': contentType } };
         let data = Axios.post<T>(url, obj,config);
-        //console.log(`Add >> ${data}`);
+        console.log(`Add >> ${data}`);
         return data;
     }
     Update<T>(obj:T, contentType:string,url:string): Promise<AxiosResponse>  {
         const config = { headers: { 'Content-Type': contentType,  'dataType': 'json'}};
         let data = Axios.put<T>(url, obj,config);
-        //console.log(`Update >> ${data}`);
+        console.log(`Update >> ${data}`);
         return data;
     }
     Remove(contentType:string,url:string): Promise<AxiosResponse>{
         const config = { headers: { 'Content-Type': contentType } };
         let data = Axios.delete(url,config);
-        //console.log(`Remove >> ${data}`);
+        console.log(`Remove >> ${data}`);
         return data;
     }
-    HandleMethods(responseAx:Promise<AxiosResponse>,spinner: Spinner): void    {
+    HandleMethods(responseAx:Promise<AxiosResponse>): void    {
         responseAx.then((response:AxiosResponse) : void => {
-            this.utility.stopSpinner(spinner);
-            //this.utility.hidediv(this.constants.AppVariables.SpinnerId);
             this.handleResponse(response); 
         }).catch( (error) =>  {           
             if (error.response) {
@@ -55,13 +48,7 @@ class BaseDataService implements IBaseDataService{
                     //empty
                 }
                 else {
-                    let errors = this.getErrors(error.response.data);
-                    if (errors && errors.length > 0) {
-                        this.handleError(errors);
-                    }
-                    else{ 
-                        //empty 
-                    }
+                    console.log(error.response.data);
                 }
             }
             else if (error.request) {
@@ -74,26 +61,18 @@ class BaseDataService implements IBaseDataService{
                 // Something happened in setting up the request and triggered an Error 
             }
             //const json= JSON.stringify(responseError.response.data, null, 4);
-            this.utility.stopSpinner(spinner); 
         });
 
         
     }
-    handleResponse(response:AxiosResponse) : void  { 
-        
+    handleResponse(response:AxiosResponse) : void  {
+        console.log(response.data);
     }
-    handleError(errors: ValidationError[]):void {
-       
-    }
-    getErrors(data: any) : ValidationError[] {
-        let list: ValidationError[]=[];
-        for(let result of data){
-            let serverError: ValidationError;
-            serverError = new ValidationError(result.name, result.message);
-            list.push(serverError);
-        }
-        return list;
-    }
+
+
 }
 
 export {IBaseDataService,BaseDataService}
+/*
+
+ */
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,13 +1,32 @@
 import * as toastr from "toastr";
-
+import Swal from "sweetalert2";
 import { ApplicationEvents } from "./helpers/applicationevents";
-
+import {BaseDataService, IBaseDataService} from "./helpers/base.dataservice";
+import {EmployeesModel} from "./models/employees.model";
+import {AxiosResponse} from "axios";
 class Main {
+    dataService:IBaseDataService;
     constructor() {
+        this.dataService = new BaseDataService();
+
     }
     init() {
         toastr.options.positionClass = 'toast-bottom-full-width'
         toastr.info("This is the information","Title Toaster");
+        let employees: Array<EmployeesModel>;
+        this.dataService.Get(`https://nwwebapi.azurewebsites.net/api/Employees`)!
+            .then((axiosResponse: AxiosResponse<Array<EmployeesModel>>) => {
+                employees = axiosResponse.data;
+                 console.log(axiosResponse.data);
+                for (let i=0;i<employees.length; i++){
+                    console.log(`${employees[i].lastName}, ${employees[i].firstName}`);
+                }
+            }).catch((error) =>{
+                console.log(error);
+            }).finally(() =>{
+                console.log("finally method called. all done!");
+                Swal.fire(`Employees `, `employess record count is ${employees.length}`, "info");
+           });
     }
 }
 export {Main}
